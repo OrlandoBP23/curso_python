9a116149cba559542b56c547d30004f988da2110
OrlandoBP23 <orlandito.bjz@gmail.com>
2025-01-24 10:58:52 -0700

    funciones

--- linea/funciones.py
+++ linea/funciones.py
@@ -1,25 +1,32 @@
-#archivo necesario con todas las funciones para la aplicacion de lineas
-
-def calcular_y(x,m,b):
+def Calcular_Y(x,m,b):
     '''
-    Calcula el valor de y en una linea recta
+    Calcula el valor de y en una linea recta 
     x: valor de x
     m: pendiente
-    b: interseccion en y
+    b: interseccion de y
     regresa el valor de y
     '''
-    return m*x +b
+    return m*x+b
+
+def main():
+    m=2
+    b=3
+    x=5
+    y= Calcular_Y(x,m,b)
+    print(f'Para x={x}, y={y}')
+
+if __name__ == '__main__':
+    main()
 
 def test_linea():
     '''
-    Prueba de funcionamiento de calcular_y
+    Prueba de funcionamiento de calcular_Y
     '''
-    assert calcular_y(0,2,3) == 3
+    y = Calcular_Y(0,2,3)
+    return y
 
 if __name__ == '__main__':
-   if test_linea():
-    print('Todo bien')
-   else:
-      print('Todo mal')
-      
-
+    if test_linea() == 3:
+        print('everything aight')
+    else:
+        print('sum went wrong')
