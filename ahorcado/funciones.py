c296d7dc64c478264ca195f2b5533632bb8d336b
OrlandoBP23 <orlandito.bjz@gmail.com>
2025-02-11 10:53:41 -0700

    se agregaron los datos, y las funciones de para leer estos archivos

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -7,11 +7,34 @@ def carga_archivo_texto(archivo:str)-> list:
     Carga un archivo de texto y devuelveuna lista con las oraciones del archivo
     '''
 
-    with open(archivo,'r') as file:
+    with open(archivo, 'r', encoding='utf-8') as file:
         oraciones = file.readlines()
-    return oraciones    
+    return oraciones  
+
+def carga_plantillas(nombre_plantilla)->dict:
+    '''
+    Carga las plantillas del juego a partir de un archivo de un texto
+    '''  
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una planitilla del juego
+    '''
+    if nivel <=5:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
 
 if __name__ == '__main__':
-    lista = carga_archivo_texto('./plantillas/plantilla-0.txt')
-    for elemento in lista:
-        print(elemento)
+    plantilla = carga_plantillas('plantilla')
+    despliega_plantilla(plantilla,5)
+    lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+    print(lista_oraciones[50:60])
+    texto = "".join(lista_oraciones[110:])
+    print(texto[:100])
+    
+
